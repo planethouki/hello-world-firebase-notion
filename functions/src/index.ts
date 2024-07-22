044306afb12c7e8c76ebf1b8cbce5f7aafb9f302
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-22 22:44:51 +0900

    sample functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// ルートハンドラの定義
+app.get('/hello', (req: any, res: any) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  res.send('Hello from Firebase Functions with Express!');
 });
+
+exports.api = functions.region('asia-northeast1').https.onRequest(app);
