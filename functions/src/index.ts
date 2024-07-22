fc977493f774c2f91adea7c5fcd98b3bb2979740
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-23 08:41:15 +0900

    functions test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,14 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-// ルートハンドラの定義
+app.get('/', (req: any, res: any) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  res.send('/ Hello from Firebase Functions with Express!');
+});
+
 app.get('/hello', (req: any, res: any) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  res.send('Hello from Firebase Functions with Express!');
+  res.send('/hello Hello from Firebase Functions with Express!');
 });
 
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
