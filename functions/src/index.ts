9f1eb2b36d8cbd614f32218dfff1f3697f7c1816
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-23 09:08:53 +0900

    functions test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,4 +12,9 @@ app.get('/hello', (req: any, res: any) => {
   res.send('/hello Hello from Firebase Functions with Express!');
 });
 
+app.get('/api/hello', (req: any, res: any) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  res.send('/api/hello Hello from Firebase Functions with Express!');
+});
+
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
