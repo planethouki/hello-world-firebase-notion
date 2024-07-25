843b43067aec5b215f558b61141d4350f4a2ec39
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-25 10:16:53 +0900

    client add login view

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup>
+const clientId = import.meta.env.VITE_NOTION_CLIENT_ID
+const redirectUri = import.meta.env.VITE_NOTION_REDIRECT_URI
+const loginUrl = `https://api.notion.com/v1/oauth/authorize?client_id=${clientId}&response_type=code&owner=user&redirect_uri=${encodeURIComponent(redirectUri)}`
+</script>
+
+<template>
+  <main>
+    <section>
+      <h1>Login</h1>
+      <a :href="loginUrl">
+        Login with Notion
+      </a>
+    </section>
+  </main>
+</template>
