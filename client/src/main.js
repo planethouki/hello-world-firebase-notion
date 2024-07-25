ded2797dc7d62d27b21efdc090af0e1d74f20b1c
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-25 10:16:17 +0900

    client initialize firebase

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from 'firebase/app'
+
+initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG))
+console.log('firebase initialized')
 
 const app = createApp(App)
 
